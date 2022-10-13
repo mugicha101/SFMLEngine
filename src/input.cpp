9cfb4b75636449e8182888d5af9a069402db2ff6
Alexander Yoshida <ayoshida@unc.edu>
2022-10-13 02:25:26 -0400

    fixes

--- src/input.cpp
+++ src/input.cpp
@@ -4,11 +4,32 @@
 #include <SFML/Graphics.hpp>
 
 class Input {
+private:
+	static Input nullInput;
+	inline static std::unordered_map<std::string, Input> idInputMap;
+	inline static std::unordered_map<sf::Keyboard::Key, Input*> keyInputMap;
+	inline static std::vector<std::pair<sf::Keyboard::Key, bool>> events;
+
+	static Input& get(std::string id) {
+		return idInputMap.count(id) == 0 ? idInputMap[id] : nullInput;
+	}
+
+	bool pressed;
+	bool just;
+
+	void tick() {
+		just = false;
+	}
+
+	void update(bool newPressedState) {
+		just = newPressedState != pressed;
+		pressed = newPressedState;
+	}
 public:
 	Input() : pressed(false), just(false) {}
 
 	static void inputEvent(sf::Keyboard::Key key, bool pressed) {
-		KeyEvent::events.emplace_back(key, pressed);
+		events.emplace_back(key, pressed);
 	}
 
 	// advance state by a tick
@@ -18,11 +39,11 @@ public:
 			kvp.second.tick();
 
 		// update state of inputs that got updated
-		for (const KeyEvent& e : KeyEvent::events) {
-			if (keyInputMap.find(e.key) == keyInputMap.end()) continue;
-			keyInputMap[e.key]->update(e.pressed);
+		for (const auto& e : events) {
+			if (keyInputMap.find(e.first) == keyInputMap.end()) continue;
+			keyInputMap[e.first]->update(e.second);
 		}
-		KeyEvent::events.clear();
+		events.clear();
 	}
 
 	// maps a key to an input (note: only one input per key allowed)
@@ -50,34 +71,5 @@ public:
 
 	static bool justChanged(std::string id) {
 		return get(id).just;
-	}
-private:
-	struct KeyEvent {
-		static std::vector<KeyEvent> events;
-
-		sf::Keyboard::Key key;
-		bool pressed;
-
-		KeyEvent(sf::Keyboard::Key key, bool pressed) : key(key), pressed(pressed) {}
-	};
-
-	static Input nullInput;
-	static std::unordered_map<std::string, Input> idInputMap;
-	static std::unordered_map<sf::Keyboard::Key, Input*> keyInputMap;
-
-	static Input& get(std::string id) {
-		return idInputMap.count(id) == 0 ? idInputMap[id] : nullInput;
-	}
-
-	bool pressed;
-	bool just;
-
-	void tick() {
-		just = false;
-	}
-
-	void update(bool newPressedState) {
-		just = newPressedState != pressed;
-		pressed = newPressedState;
 	}
 };
