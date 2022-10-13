c89b0a85517f49cc8d34e7f46b4af629d20d1a7a
Alexander Yoshida <ayoshida@unc.edu>
2022-10-13 13:45:09 -0400

    incomplete type fix

--- src/input.cpp
+++ src/input.cpp
@@ -5,28 +5,32 @@
 
 class Input {
 private:
-	inline static std::unordered_map<std::string, Input> idInputMap;
-	inline static std::unordered_map<sf::Keyboard::Key, Input*> keyInputMap;
+	struct Data {
+		bool pressed;
+		bool just;
+		
+		Data() : pressed(false), just(false) {}
+
+		void tick() {
+			just = false;
+		}
+
+		void update(bool newPressedState) {
+			just = newPressedState != pressed;
+			pressed = newPressedState;
+		}
+	};
+
+	static Data nullData;
+
+	inline static std::unordered_map<std::string, Data> idMap;
+	inline static std::unordered_map<sf::Keyboard::Key, Data*> keyMap;
 	inline static std::vector<std::pair<sf::Keyboard::Key, bool>> events;
 
-	static Input& get(std::string id) {
-		return idInputMap[id];
-	}
-
-	bool pressed;
-	bool just;
-
-	void tick() {
-		just = false;
-	}
-
-	void update(bool newPressedState) {
-		just = newPressedState != pressed;
-		pressed = newPressedState;
+	static Data& get(std::string id) {
+		return idMap.count(id) == 0 ? nullData : idMap[id];
 	}
 public:
-	Input() : pressed(false), just(false) {}
-
 	static void inputEvent(sf::Keyboard::Key key, bool pressed) {
 		events.emplace_back(key, pressed);
 	}
@@ -34,43 +38,41 @@ public:
 	// advance state by a tick
 	static void inputTick() {
 		// change just to false
-		for (auto& kvp : idInputMap)
+		for (auto& kvp : idMap)
 			kvp.second.tick();
 
 		// update state of inputs that got updated
 		for (const auto& e : events) {
-			if (keyInputMap.find(e.first) == keyInputMap.end()) continue;
-			keyInputMap[e.first]->update(e.second);
+			if (keyMap.find(e.first) == keyMap.end()) continue;
+			keyMap[e.first]->update(e.second);
 		}
 		events.clear();
 	}
 
 	// maps a key to an input (note: only one input per key allowed)
 	static void mapInput(sf::Keyboard::Key key, std::string id) {
-		keyInputMap[key] = &idInputMap[id];
+		keyMap[key] = &idMap[id];
 	}
 
 	static bool isPressed(std::string id) {
-		return idInputMap.count(id) == 0 ? false : get(id).pressed;
+		return get(id).pressed;
 	}
 
 	static bool isReleased(std::string id) {
-		return idInputMap.count(id) == 0 ? true : !get(id).pressed;
+		return !get(id).pressed;
 	}
 
 	static bool justPressed(std::string id) {
-		if (idInputMap.count(id) == 0) return false;
-		Input& input = get(id);
+		Data& input = get(id);
 		return input.pressed && input.just;
 	}
 
 	static bool justReleased(std::string id) {
-		if (idInputMap.count(id) == 0) return false;
-		Input& input = get(id);
+		Data& input = get(id);
 		return !input.pressed && input.just;
 	}
 
 	static bool justChanged(std::string id) {
-		return idInputMap.count(id) == 0 ? false : get(id).just;
+		return get(id).just;
 	}
 };
