48670fa92da23bd3e9949f46ccf463420965a660
Alexander Yoshida <ayoshida@unc.edu>
2022-10-15 16:18:00 -0400

    seperated scenegraph into header

---
+++ src/scenegraph.h
@@ -0,0 +1,23 @@
+#include "./nodes.cpp"
+
+// scene graph
+class SceneGraph {
+private:
+public:
+	std::shared_ptr<Node> root;
+	sf::RenderTarget* renderTarget;
+
+	SceneGraph(sf::RenderTarget& renderTarget) {
+		this->renderTarget = &renderTarget;
+		root = std::make_shared<Node>();
+	}
+
+	void drawTick(int calcTick) {
+		renderTarget->clear();
+		root->draw(*renderTarget, sf::Transform::Identity, calcTick);
+	}
+
+	static std::shared_ptr<Node> create() {
+		return std::make_shared<Node>();
+	}
+};
